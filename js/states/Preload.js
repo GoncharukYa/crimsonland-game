ee8ca802de3566b5cdeaf6baa228fdd70eb06378
Goncharuk Yaroslav <gonyarik@gmail.com>
2017-12-23 01:02:13 +0300

    Fix maybe all paths

--- js/states/Preload.js
+++ js/states/Preload.js
@@ -12,16 +12,16 @@ class Preload extends Phaser.State {
         loadingBar.anchor.setTo(0.5,0.5);
         this.game.load.setPreloadSprite(loadingBar);
         
-		this.game.load.atlas('player-body-handgun', '/assets/player/handgun/move/survivor-move_handgun.png', '../assets/player/handgun/move/survivor-move_handgun.json');
-        this.game.load.atlas('player-feet', '/assets/player/feet/feet.png', '../assets/player/feet/feet.json');
-        this.game.load.image('bullet', '/assets/player/shmup-bullet.png');
-        this.game.load.atlas('zombie1', '/assets/zombie/zombie1/zombie1.png', '../assets/zombie/zombie1/zombie1.json');
-        this.game.load.image('earth', '/assets/scorched_earth.png');
-        this.game.load.image('blood', '/assets/blood.png');
-        this.game.load.audio('theme', '/assets/music/theme.mp3');
-        this.game.load.audio('pistol-shot', '/assets/shots/pistol.wav');
-        this.game.load.audio('zombie-pain', '/assets/music/zombie_pain.mp3');
-        this.game.load.bitmapFont('upheaval', '/assets/fonts/upheaval/font.png', '/assets/fonts/upheaval/font.fnt');
+		this.game.load.atlas('player-body-handgun', 'assets/player/handgun/move/survivor-move_handgun.png', 'assets/player/handgun/move/survivor-move_handgun.json');
+        this.game.load.atlas('player-feet', 'assets/player/feet/feet.png', 'assets/player/feet/feet.json');
+        this.game.load.image('bullet', 'assets/player/shmup-bullet.png');
+        this.game.load.atlas('zombie1', 'assets/zombie/zombie1/zombie1.png', 'assets/zombie/zombie1/zombie1.json');
+        this.game.load.image('earth', 'assets/scorched_earth.png');
+        this.game.load.image('blood', 'assets/blood.png');
+        this.game.load.audio('theme', 'assets/music/theme.mp3');
+        this.game.load.audio('pistol-shot', 'assets/shots/pistol.wav');
+        this.game.load.audio('zombie-pain', 'assets/music/zombie_pain.mp3');
+        this.game.load.bitmapFont('upheaval', 'assets/fonts/upheaval/font.png', 'assets/fonts/upheaval/font.fnt');
 	
 
 
